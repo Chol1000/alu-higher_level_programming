2fee7b0d45c436c373c514df784fb47b1b7703b2
Chol1000 <c.monykuch@alustudent.com>
2024-06-19 03:08:19 -0700

    print integer

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 try:
+    # Variable containing the integer to be printed
     number = 98
+
+    # Printing the integer followed by "Battery street" using f-string
     print(f"{number} Battery street")
+
 except Exception as e:
-    print("Error occurred:", e)
+    print(f"An error occurred: {e}")
