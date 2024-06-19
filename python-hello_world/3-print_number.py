363b5d5dc77644f2083442731781e16fe8655140
Chol1000 <c.monykuch@alustudent.com>
2024-06-19 04:37:50 -0700

    print integer

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-try:
-    number=98
-    print(f"{number} Battery street")
-except NameError:
-    print("Variable 'number' is not defined.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+number = 98
+print(f"{number:d} Battery street")
