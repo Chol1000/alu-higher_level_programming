85f0e62131986f5b460754f39be5c5dac07c03a1
Chol1000 <c.monykuch@alustudent.com>
2024-06-19 02:32:09 -0700

    print number

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -2,6 +2,7 @@
 number = 98  
 
 try:
+    # Ensure the value is an integer
     if not isinstance(number, int):
         raise TypeError("number must be an integer")
     print(f"{number} Battery street")
