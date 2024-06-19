c0a107c5f7871195d74e622205b8b4f65802cb73
Chol1000 <c.monykuch@alustudent.com>
2024-06-19 03:05:22 -0700

    print number

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 try:
-    
     number = 98
-
-    # Printing the integer followed by a specific string using f-string
     print(f"{number} Battery street")
-
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print("Error occurred:", e)
