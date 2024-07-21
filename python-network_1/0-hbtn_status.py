2d18710f53ee9ecf64744c20b2e6e35225b57ecd
Chol1000 <c.monykuch@alustudent.com>
2024-07-21 16:22:13 -0700

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""This module or script
-fetches https://alu-intranet.hbtn.io/status
-using the urllib library
+"""
+A Python script that fetches https://alu-intranet.hbtn.io/status
+using urllib package and displays specific details about the response.
 """
 
+import urllib.request
+import urllib.error
 
-from urllib import request, error
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
 
-try:
-    url = 'https://intranet.hbtn.io/status'
-    with request.urlopen(url) as response:
-        response_body = response.read()
-        response_text = response_body.decode('utf-8')
-        print("Body response:")
-        print("\t- type:", type(response_body))
-        print("\t- content:", response_body)
-        print("\t- utf8 content:", response_text)
-except error.URLError:
-    custom_status = bytes("Custom status", encoding="utf-8")
-    custom_status_text = custom_status.decode('utf-8')
-    print("Body response:")
-    print("\t- type:", type(custom_status))
-    print("\t- content:", custom_status)
-    print("\t- utf8 content:", custom_status_text)
+    try:
+        with urllib.request.urlopen(url) as response:
+            html_content = response.read()
+            utf8_content = html_content.decode('utf-8')
+
+            print("Body response:")
+            print("\t- type: {}".format(type(html_content)))
+            print("\t- content: {}".format(html_content))
+            print("\t- utf8 content: {}".format(utf8_content))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+
