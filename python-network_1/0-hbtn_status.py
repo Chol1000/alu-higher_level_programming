5e7e4f8a08515f6d9622a6d1950692afc9946cfe
Chol1000 <c.monykuch@alustudent.com>
2024-07-21 16:25:28 -0700

    Task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-A Python script that fetches https://alu-intranet.hbtn.io/status
-using urllib package and displays specific details about the response.
+"""A script
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
 
-import urllib.request
-import urllib.error
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    import urllib.request
 
-    try:
-        with urllib.request.urlopen(url) as response:
-            html_content = response.read()
-            utf8_content = html_content.decode('utf-8')
-
-            print("Body response:")
-            print("\t- type: {}".format(type(html_content)))
-            print("\t- content: {}".format(html_content))
-            print("\t- utf8 content: {}".format(utf8_content))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
