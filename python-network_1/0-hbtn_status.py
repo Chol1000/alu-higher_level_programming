584ae202bfbdb3181cf9f0e780f2c6fc61b7e2e1
Chol1000 <c.monykuch@alustudent.com>
2024-07-21 16:35:10 -0700

    Fix 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""A script
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
-
+"""get https://alu-intranet.hbtn.io/status"""
+from urllib import request, error
 
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    url = "https://alu-intranet.hbtn.io/status"
+    try:
+        with request.urlopen(url) as response:
+            response = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(response)))
+            print("\t- content: {}".format(response))
+            print("\t- utf8 content: {}".format(response.decode('utf-8')))
+    except error.URLError:
+        print("Cannot connect to https://alu-intranet.hbtn.io/status")
