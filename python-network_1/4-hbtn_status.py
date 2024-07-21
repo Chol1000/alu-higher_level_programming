37b656a7ad4b458832295f259a20210b47eb648a
Chol1000 <c.monykuch@alustudent.com>
2024-07-21 16:28:01 -0700

    4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that
-- fetches https://alu-intranet.hbtn.io/status.
+"""This module or script
+uses the requests package
+to fetch https://alu-intranet.hbtn.io/status
+and displays the body of the response.
 """
+
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
